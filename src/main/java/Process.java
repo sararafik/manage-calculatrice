080d82c363834d4992b23998d4bdaa58529dc29c
DESKTOP-L6C90VG\admin <rafiksara1@gmail.com>
2022-01-17 13:55:49 +0100

    add project ManageTest

---
+++ src/main/java/Process.java
@@ -0,0 +1,11 @@
+
+
+public class Process {
+    public static void main(String[] args) {
+        int[] array = new int[] {0,1,2,3,4,5,3};
+        System.out.println(Solution.calc(array,0,1));
+        System.out.println(Solution.calc(array,0,5));
+        System.out.println(Solution.calc(array,0,0));
+        System.out.println(Solution.calc(array,0,6));
+    }
+}
---
+++ src/main/java/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    public static  int calc(int[] array, int n1, int n2){
+        int somme = 0;
+        for(int i=0;i<array.length;i++)
+        {
+            if(i>=n1 && i<=n2)
+            {
+                somme = somme + array[i];
+            }
+        }
+        return somme;
+    }
+
+}
