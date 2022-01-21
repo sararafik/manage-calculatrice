6bf526395e5920b8cd234f0f3cc6fcc003704791
DESKTOP-L6C90VG\admin <rafiksara1@gmail.com>
2022-01-21 18:10:53 +0100

    modify TestUnitaireFile + add assertEquals

--- src/test/java/CalculatriceTest.java
+++ src/test/java/CalculatriceTest.java
@@ -1,11 +1,22 @@
+
+
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CalculatriceTest {
 
     @Test
     void testCalculatrice() {
-
-
+        int[] array = new int[] {0,1,2,3,4,5,3};
+        assertEquals(1,Solution.calc(array,0,1));
+        assertEquals(15,Solution.calc(array,0,5));
+        assertEquals(0,Solution.calc(array,0,0));
+        assertEquals(18,Solution.calc(array,0,6));
 
     }
+
+
+
 }
